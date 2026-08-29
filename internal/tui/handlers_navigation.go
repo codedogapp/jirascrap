119bca3324445b15d2a044ff63b94e51fbff3c1e
Bakhomious Basta <bakhomioussvp@gmail.com>
2026-08-29 12:45:34 +0200

    fix nosec

--- internal/tui/handlers_navigation.go
+++ internal/tui/handlers_navigation.go
@@ -265,9 +265,8 @@ func (m *AppModel) handleOpenInBrowser(msg tea.KeyPressMsg) (bool, tea.Cmd) {
 	ticketURL := fmt.Sprintf("%s/browse/%s", domain, ticket.ID)
 
 	return true, func() tea.Msg {
-		_ = exec.Command("open", ticketURL).
-			Start()
-			// #nosec G204 -- ticketURL is from trusted config
+		// #nosec G204 -- ticketURL is from trusted config + ticket ID
+		_ = exec.Command("open", ticketURL).Start()
 		return nil
 	}
 }
